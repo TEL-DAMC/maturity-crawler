2b13ade461ced6be3ebc3088ceb2226b7a8afb06
R.Frega <robertofrega@gmail.com>
2018-03-20 19:40:17 -0300

    Refatora os arquivos principais

--- engine/methods.js
+++ engine/methods.js
@@ -1,193 +1,212 @@
-#!/usr/local/bin/node
-
 /* LIBS */
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 /* VARS */
-var prop = {};
+const pageTestResults = {}
 
 /* METHODS */
-methods = {}
+const methods = {}
 
-// check arguments (useless?)
-methods.check_args = function(p_args, t_chk) {
-    var args = require('minimist')(p_args);
-    if (!(t_chk in args)) {
-      console.log('There is no env. Please use "node run.js --env example.com"');
-      process.exit();
-    }
-}
+let globalBrowserInstance
 
 // get parameter name
-methods.get_param_name = function(name, url) {
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+methods.getUrlParamValueFromName = function (url, name) {
+  name = name.replace(/[\[\]]/g, '\\$&')
+  let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  let results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
 // fetch pages
-methods.get_lps = function(lps, sc){
-    for (var i in lps) {
-        prop[lps[i].endpoint] = {
-            endpoint: lps[i].endpoint,
-            gtms: [],
-            gtms_ok: false,
-            uas: [],
-            uas_ok: false,
-            data_layers: [],
-            data_layers_ok: false,
-            keys_log: [],
-            gtm_position: '-'
-        };
-        this.fetch(lps[i], sc).then(function(){
-            console.log(JSON.stringify(prop));
-        }).catch(function(e){
-            console.log(e);
-        });
-    }
-}
-
-methods.fetch = async function(lp, sc) {
-    // open browser
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // activate the sniffer
-    page.on('response', response => {
-        req = response.request();
-
-        if (req.hasOwnProperty('url')) {
-            // to see all network requests
-            // console.log(req.url);
-
-            // get gtm
-            if (req.url.indexOf('gtm.js') > -1) {
-                this.check_gtm(req.url, lp);
-            }
-
-            // get GA
-            if (req.url.indexOf('/collect') > -1) {
-                if (this.get_param_name('t', req.url) == 'pageview') {
-                    this.check_ua(req.url, lp);
-                }
-            }
-        }
-    });
-
-    // handling error
-    page.on('error', msg => { browser.close(); });
-    page.on('pageerror', msg => { browser.close(); });
-
-    // go to env
-    const rsp = await page.goto(lp.endpoint, {waitUntil: 'networkidle0'});
-
-    // html content analysis
-    pg_html = await page.content();
-    this.check_gtm_position(lp, pg_html);
-
-    // check DataLayer
-    pg_data_layer = await page.evaluate((gtm_dl) => window[gtm_dl], (prop[lp.endpoint].data_layers[0]));
-    this.check_keys(lp, JSON.stringify(pg_data_layer));
-
-    await browser.close();
-
-    this.gtms_ok(lp);
-    this.uas_ok(lp);
-    this.data_layers_ok(lp);
-
-    this.write_sheet(lp, sc);
-}
-
-methods.check_gtm = function(url, lp) {
-    prop[lp.endpoint].gtms.push(this.get_param_name('id', url));
-    prop[lp.endpoint].data_layers.push(this.get_param_name('l', url));
-}
-
-methods.check_ua = function(url, lp) {
-    prop[lp.endpoint].uas.push(this.get_param_name('tid', url));
-}
-
-methods.check_gtm_position = function(lp, pg_html) {
-    pg_gtm = pg_html.indexOf('gtm.js?');
-
-    if (pg_gtm > -1) {
-        if (pg_gtm < pg_html.indexOf('</head')) prop[lp.endpoint].gtm_position = 'header';
-        if (pg_gtm > pg_html.indexOf('<body')) {
-            to_s = Math.abs(pg_html.indexOf('<body') - pg_gtm);
-            to_e = pg_html.indexOf('</body') - pg_gtm;
-            if (to_s > to_e) {
-                prop[lp.endpoint].gtm_position = 'body bottom';
-            } else {
-                prop[lp.endpoint].gtm_position = 'body init';
-            }
-        }
-    }
-}
-
-methods.gtms_ok = function(lp) {
-    // check gtms
-    if (lp.gtms.sort().join(',') == prop[lp.endpoint].gtms.sort().join(',')) {
-        prop[lp.endpoint].gtms_ok = true;
+methods.checkLandingPages = async function (landingPages, googleSheetsConfig) {
+  globalBrowserInstance = await puppeteer.launch({headless: false})
+  let currentlyRunning = 0
+  ;(async function loop (i) {
+    if (currentlyRunning < 8) {
+      currentlyRunning++
+      methods.fetch(landingPages[i], googleSheetsConfig).catch(e => console.warn('fetch error:', landingPages[i], e))
     } else {
-        prop[lp.endpoint].gtms_ok = false;
+      await methods.fetch(landingPages[i], googleSheetsConfig).catch(e => console.warn('fetch error:', landingPages[i], e))
+      currentlyRunning = (await globalBrowserInstance.pages()).length
     }
+    i++
+    await loop(i)
+  })(0)
+
+  for (let i in landingPages) {
+    if (landingPages.hasOwnProperty(i)) {
+      pageTestResults[landingPages[i].endpoint] = {
+        endpoint: landingPages[i].endpoint,
+        gtms: [], // GTM IDs
+        gtms_ok: false,
+        uas: [], // UA Tracking IDs
+        uas_ok: false,
+        data_layers: [], // GTM dataLayer object names
+        data_layers_ok: false,
+        keys_log: [],
+        gtm_position: '-'
+      }
+    }
+  }
 }
 
-methods.uas_ok = function(lp) {
-    // check uas
-    if (lp.uas.sort().join(',') == prop[lp.endpoint].uas.sort().join(',')) {
-        prop[lp.endpoint].uas_ok = true;
+methods.fetch = async function (lp, sc) {
+  // open browser
+  const chromeBrowserInstance = globalBrowserInstance
+
+  if (!lp || !lp.endpoint) return
+  const chomeTab = await chromeBrowserInstance.newPage()
+
+  // activate the sniffer
+  chomeTab.on('response', response => {
+    let url = response.url()
+
+    if (url) {
+      // to see all network requests
+      // console.log(req.url);
+      // get gtm
+      if (url.indexOf('gtm.js') > -1) {
+        let gtmInfo = this.getGtmInfoFromUrl(url)
+
+        pageTestResults[lp.endpoint].gtms.push(gtmInfo.gtmId)
+        pageTestResults[lp.endpoint].data_layers.push(gtmInfo.dataLayerObjectName)
+      } else if (url.indexOf('/collect') > -1 && this.getUrlParamValueFromName(url, 't') === 'pageview') {
+        pageTestResults[lp.endpoint].uas.push(this.getUaTid(url))
+      }
+    }
+  })
+
+  // handling error
+  chomeTab.on('error', msg => {
+    console.warn('browser error', lp.endpoint)
+  })
+
+  chomeTab.on('pageerror', msg => {
+    console.warn('page js error', lp.endpoint)
+  })
+
+  console.log('endpoint:', lp.endpoint)
+  await chomeTab.goto(lp.endpoint, {waitUntil: 'networkidle0'}).catch(e => {
+    console.error('error on ', lp.endpoint, ':', e.message)
+  })
+
+  // html content analysis
+  const PageHTML = await chomeTab.content()
+  console.log('html length:', PageHTML.length)
+
+  pageTestResults[lp.endpoint].gtm_position = methods.checkGtmPositionStatus(PageHTML)
+
+  console.log(pageTestResults[lp.endpoint].data_layers)
+  const PageDataLayer = await methods.getDataLayerObject(chomeTab, pageTestResults[lp.endpoint].data_layers[0])
+
+  methods.checkObjectKeys(lp, JSON.stringify(PageDataLayer))
+
+  if ((await chromeBrowserInstance.pages()).length > 2) await chomeTab.close()
+
+  pageTestResults[lp.endpoint].gtms_ok = methods.gtms_ok(lp)
+  methods.uas_ok(lp)
+  methods.dataLayersOk(lp)
+
+  methods.writeSpreadsheet(lp, sc)
+}
+
+methods.getDataLayerObject = async function (chromeTab, dataLayerName) {
+  return chromeTab.evaluate((GtmDataLayerName) => {
+    return window[GtmDataLayerName]
+  }, dataLayerName)
+}
+/**
+ * Returns GTM's dataLayer name and id from its url
+ * @param url GTM js URL
+ * @returns {{dataLayerObjectName: string, gtmId: string}}
+ */
+methods.getGtmInfoFromUrl = function (url) {
+  return {
+    dataLayerObjectName: methods.getUrlParamValueFromName(url, 'l'),
+    gtmId: methods.getUrlParamValueFromName(url, 'id')
+  }
+}
+
+/**
+ * Returns Google Analytics Tracking ID from the url
+ * @param url
+ * @returns {string} Tracking Id
+ */
+methods.getUaTid = function (url) {
+  return this.getUrlParamValueFromName(url, 'tid')
+}
+
+methods.checkGtmPositionStatus = function (pageHtml) {
+  let gtmPositionStatus
+
+  const gtmPositionIndexOnPage = pageHtml.indexOf('gtm.js?') // TODO: Check more than one GTM on the same page
+
+  if (gtmPositionIndexOnPage <= -1) {
+    gtmPositionStatus = 'not found'
+  } else if (gtmPositionIndexOnPage < pageHtml.indexOf('</head')) {
+    gtmPositionStatus = 'header'
+  } else if (gtmPositionIndexOnPage > pageHtml.indexOf('<body')) {
+    const distanceToStart = Math.abs(pageHtml.indexOf('<body') - gtmPositionIndexOnPage)
+    const distanceToEnd = pageHtml.indexOf('</body') - gtmPositionIndexOnPage
+    if (distanceToStart > distanceToEnd) {
+      gtmPositionStatus = 'body bottom'
     } else {
-        prop[lp.endpoint].uas_ok = false;
+      gtmPositionStatus = 'body init'
     }
+  }
+  return gtmPositionStatus
 }
 
-methods.data_layers_ok = function(lp) {
-    // check data_layers
-    if (lp.data_layers.sort().join(',') == prop[lp.endpoint].data_layers.sort().join(',')) {
-        prop[lp.endpoint].data_layers_ok = true;
-    } else {
-        prop[lp.endpoint].data_layers_ok = false;
-    }
+methods.gtms_ok = function (lp) {
+  // check gtms
+  return lp.gtms.sort().join(',') === pageTestResults[lp.endpoint].gtms.sort().join(',')
 }
 
-methods.check_keys = function(lp, json) {
-    for (k of lp.keys) {
-        if (json.indexOf(k) < 0) {
-            prop[lp.endpoint].keys_log.push(k);
-        }
-    }
+methods.uas_ok = function (lp) {
+  // check uas
+  pageTestResults[lp.endpoint].uas_ok = lp.uas.sort().join(',') === pageTestResults[lp.endpoint].uas.sort().join(',')
 }
 
-methods.write_sheet = function(lp, sc) {
+methods.dataLayersOk = function (lp) {
+  // check data_layers
+  pageTestResults[lp.endpoint].dataLayersOk = lp.data_layers.sort().join(',') === pageTestResults[lp.endpoint].data_layers.sort().join(',')
+}
+
+methods.checkObjectKeys = function (lp, json) {
+  for (let key of lp.keys) {
+    if (json.indexOf(key) < 0) { // todo: dangerous implementation of key check on stringified JSON
+      pageTestResults[lp.endpoint].keys_log.push(key)
+    }
+  }
+}
+
+methods.writeSpreadsheet = function (lp, sc) {
   sc.sheets.spreadsheets.values.append({
-      auth: sc.auth,
-      spreadsheetId: sc.spreadsheet_id,
-      range: 'maturity!G' + lp.row.toString() + ':M'+ lp.row.toString(),
-      valueInputOption: 'USER_ENTERED',
-      resource: {
-        values: [ [
-              prop[lp.endpoint].gtms.join(','), // G
-              prop[lp.endpoint].gtm_position, // H
-              prop[lp.endpoint].gtms_ok, // I
-              prop[lp.endpoint].uas.join(','), // J
-              prop[lp.endpoint].uas_ok, // K
-              prop[lp.endpoint].data_layers.join(','), // L
-              prop[lp.endpoint].keys_log.join(',') // M
-              ] ]
-      }
-    }, (err, response) => {
-      if (err) {
-        console.log('The API returned an error inserting value of ' + lp.endpoint + ':'+ err);
-        return;
-      } else {
-          console.log('Success: ' + lp.endpoint);
-      }
-  });
+    auth: sc.auth,
+    spreadsheetId: sc.spreadsheetId,
+    range: 'maturity!G' + lp.row.toString() + ':M' + lp.row.toString(),
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: [[
+        pageTestResults[lp.endpoint].gtms.join(','), // G
+        pageTestResults[lp.endpoint].gtm_position, // H
+        pageTestResults[lp.endpoint].gtms_ok, // I
+        pageTestResults[lp.endpoint].uas.join(','), // J
+        pageTestResults[lp.endpoint].uas_ok, // K
+        pageTestResults[lp.endpoint].data_layers.join(','), // L
+        pageTestResults[lp.endpoint].keys_log.join(',') // M
+      ]]
+    }
+  }, (err, response) => {
+    if (err) {
+      console.log('The API returned an error inserting value of ' + lp.endpoint + ':' + err)
+    } else {
+      console.log('Success: ' + lp.endpoint)
+    }
+  })
 }
 
 // export methods
-module.exports = methods;
+module.exports = methods
--- run.js
+++ run.js
@@ -1,77 +1,71 @@
-#!/usr/local/bin/node
-
-var gapi = require('googleapis');
-var privatekey = require('./key.json');
-var mtd = require('./engine/methods.js');
+const GoogleAPIs = require('googleapis')
+const PrivateKey = require('./key.json')
+const GlobalMethods = require('./engine/methods.js')
 
 // Google Sheets Configs
-
-var sheets_config = {
-    spreadsheet_id: '1gqiL4o8bWz1NloxgRQ7bQfZB-N4DtvrpvK2vgijZZzk',
-    sheets: gapi.google.sheets('v4'),
-    auth: undefined
+const GoogleSheetsConfig = {
+  spreadsheetId: '1gqiL4o8bWz1NloxgRQ7bQfZB-N4DtvrpvK2vgijZZzk',
+  sheets: GoogleAPIs.google.sheets('v4'),
+  auth: undefined
 }
 
-var lps = {};
+const lps = []
 
 // configure a JWT auth client
-jwt = new gapi.google.auth.JWT(
-    privatekey.client_email,
-    null,
-    privatekey.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets']
-);
+const JwtClient = new GoogleAPIs.google.auth.JWT(
+  PrivateKey.client_email,
+  null,
+  PrivateKey.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+)
 
 // authenticate request
-jwt.authorize(function (err, tokens) {
-    if (err) {
-        console.log('The API returned an error in authentication: ' + err);
-        return;
-    } else {
-        console.log('Successfully connected in spreadsheets!');
-    }
-});
-sheets_config.auth = jwt;
+JwtClient.authorize(function (err) {
+  if (err) {
+    console.log('The API returned an error in authentication: ' + err)
+  } else {
+    console.log('Successfully connected in spreadsheets!')
+  }
+})
+GoogleSheetsConfig.auth = JwtClient
 
 // clear
-sheets_config.sheets.spreadsheets.values.clear({
-    auth: sheets_config.auth,
-    spreadsheetId: sheets_config.spreadsheet_id,
-    range: 'maturity!G2:M'
+GoogleSheetsConfig.sheets.spreadsheets.values.clear({
+  auth: GoogleSheetsConfig.auth,
+  spreadsheetId: GoogleSheetsConfig.spreadsheetId,
+  range: 'maturity!G2:M'
 }, function (err, response) {
-   if (err) {
-       console.log('The API returned an error cleaning cells: ' + err);
-   } else {
-       console.log('Cells cleaned');
-   }
-});
+  if (err) {
+    console.log('The API returned an error cleaning cells: ' + err)
+  } else {
+    console.log('Cells cleaned')
+  }
+})
 
 // reading landings
-sheets_config.sheets.spreadsheets.values.get({
-    auth: sheets_config.auth,
-    spreadsheetId: sheets_config.spreadsheet_id,
-    range: 'maturity!B2:F'
+GoogleSheetsConfig.sheets.spreadsheets.values.get({
+  auth: GoogleSheetsConfig.auth,
+  spreadsheetId: GoogleSheetsConfig.spreadsheetId,
+  range: 'maturity!B2:F'
 }, function (err, response) {
-   if (err) {
-       console.log('The API returned an error retrieving infos: ' + err);
-   } else {
-       console.log('List URLs:');
-       for (var i in response.data.values) {
-            console.log(response.data.values[i][0]);
-            lps[response.data.values[i][0]] = {
-                row: parseInt(i)+2,
-                endpoint: response.data.values[i][0],
-                gtms: response.data.values[i][1].split(','),
-                uas: response.data.values[i][2].split(','),
-                data_layers: response.data.values[i][3].split(','),
-                keys: response.data.values[i][4].split(',')
-            };
-       }
+  if (err) {
+    console.log('The API returned an error retrieving infos: ' + err)
+  } else {
+    console.log('List URLs:')
+    for (let i in response.data.values) {
+      if (response.data.values.hasOwnProperty(i)) {
+        console.log(response.data.values[i][0])
+        lps.push({
+          row: parseInt(i) + 2,
+          endpoint: response.data.values[i][0],
+          gtms: response.data.values[i][1].split(','),
+          uas: response.data.values[i][2].split(','),
+          data_layers: response.data.values[i][3].split(','),
+          keys: response.data.values[i][4].split(',')
+        })
+      }
+    }
 
-       burn(lps, sheets_config);
-   }
-});
-
-var burn = function (landings, sc) {
-    mtd.get_lps(landings, sc);
-}
+    GlobalMethods.checkLandingPages(lps, GoogleSheetsConfig)
+  }
+})
